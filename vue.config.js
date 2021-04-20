3af5208e29bd176211d52e2019128cd6c9cb8c8c
ninop <nino.parolari@stud.fhgr.ch>
2021-04-20 15:14:20 +0200

    vue config angefangen

---
+++ vue.config.js
@@ -0,0 +1,30 @@
+const config = {
+    devServer: {
+        open: true,
+        disableHostCheck: true,
+    },
+    pages: {
+        index: {
+            entry: 'src/main.js',
+            template: 'public/index.html',
+            filename: 'index.html',
+        },
+        cheval: {
+            entry: 'src/main.js',
+            template: 'public/index_cheval.html',
+            filename: 'index_cheval.html',
+        },
+        bibeval: {
+            entry: 'src/main.js',
+            template: 'public/bibeval.html',
+            filename: 'index_bibeval.html',
+        },
+        suchanfrage: {
+            entry: 'src/main.js',
+            template: 'public/index_suchanfrage.html',
+            filename: 'index_suchanfrage.html',
+        },
+    },
+    // ...config code
+}
+module.exports = config
